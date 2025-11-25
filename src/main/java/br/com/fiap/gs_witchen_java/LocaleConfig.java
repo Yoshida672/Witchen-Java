9020f488f0cc9c5d4d64305e79732ec9c28072b6
Yoshida672 <ericissamuyoshida@gmail.com>
2025-11-25 12:57:15 -0300

    ajustando locale

--- src/main/java/br/com/fiap/gs_witchen_java/LocaleConfig.java
+++ src/main/java/br/com/fiap/gs_witchen_java/LocaleConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -15,9 +15,11 @@ public class LocaleConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.forLanguageTag("en"));
-        return slr;
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(Locale.forLanguageTag("en"));
+        clr.setCookieName("LOCALE");
+        clr.setCookieMaxAge(3600); // 1 hora
+        return clr;
     }
 
     @Bean
